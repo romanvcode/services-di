c695d55ebd46c030325258c679241a11ff6cd4a1
romanvcode <roman.v.ua@gmail.com>
2024-07-23 14:56:55 +0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { TasksComponent } from './tasks/tasks.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  imports: [TasksComponent],
+})
+export class AppComponent {}
---
+++ src/app/tasks/tasks.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, signal } from '@angular/core';
+import { Task } from './task.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TasksService {
+  private tasks = signal<Task[]>([]);
+
+  allTasks = this.tasks.asReadonly();
+
+  addTask(taskData: { title: string; description: string }) {
+    const newTask: Task = {
+      ...taskData,
+      id: Math.random().toString(),
+      status: 'OPEN',
+    };
+    this.tasks.update((oldTasks) => [...oldTasks, newTask]);
+  }
+
+  updateTaskStatus(taskId: string, newStatus: 'OPEN' | 'IN_PROGRESS' | 'DONE') {
+    this.tasks.update((oldTasks) =>
+      oldTasks.map((task) =>
+        task.id === taskId ? { ...task, status: newStatus } : task
+      )
+    );
+  }
+}
